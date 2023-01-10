a7a6d069ddc6b71f6b27e41df11f7d3cc23ba56f
AndreyMatviets <asmatviets@gmail.com>
2023-01-10 14:12:31 +0400

    Initial Commit

---
+++ CupcakeApp/CupcakeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeAppApp.swift
+//  CupcakeApp
+//
+//  Created by Andrey Matviets on 10.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
